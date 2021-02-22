e874189cff7c56f92eecd5c2a581bb1d5f50a5d4
jessicadelgrande <jessica.delgrande@gmail.com>
2021-02-22 13:16:47 -0500

    delete working

--- app.js
+++ app.js
@@ -8,9 +8,8 @@ firebase.initializeApp(firebaseConfig);
 
 const db = firebase.database(); 
 const dbRef = firebase.database().ref();
-// const filmsDb = dbRef.collection("films");
-// console.log("ref", dbRef);
 
+// display existing films on page
 dbRef.on('value', (data) => {
 	const filmsData = data.val();
 	const filmsArray = [];
@@ -18,17 +17,14 @@ dbRef.on('value', (data) => {
 	for (let film in filmsData) {
 		const filmItem = document.createElement("li");
 		const filmItemName = filmsData[film].name;
-		console.log("name", filmItemName);
-		// const loggedDate = dbRef.setValue(ServerValue.TIMESTAMP);
-		// console.log("logged date", loggedDate);
-		const filmItemDate = filmsData[film];
-		console.log("date", filmItemDate);
+		const filmKey = film; // used for delete function
+		const filmItemDate = filmsData[film].date;
 		filmItem.className = "filmItem";
-		// filmItem.id = filmsData[film].id; // to be able to target item for delete
+		filmItem.id = `${filmKey}`;
 		filmItem.innerHTML = `
 			 <p id="filmName">${filmItemName}</p>
 			 <p id="loggedDate">${filmItemDate}</p>
-			 <button id="removeButton">
+			 <button class="removeButton" id="">
 				 <i class="fa fa-trash-alt"></i>
 			</button>
 		`;
@@ -39,26 +35,30 @@ dbRef.on('value', (data) => {
 	filmItemContainer.innerHTML = filmsArray.join("");
 });
 
-// get data from input
+// get data from input and add to list when button is clicked
 const filmForm = document.getElementById("filmForm");
-// click handler
 filmForm.addEventListener("submit", async (e) => {
 	e.preventDefault();
 	const filmInput = document.getElementById("newFilmInput");
 	const inputValue = filmInput.value;
-	console.log("clicked");
 	
 	if (inputValue != "") {
-		console.log("value", inputValue);
 		// write to an object that can be stored in firebase
-		// const datestamp = new Date();
+		const displayDate = new Date().toDateString();
 		const filmObject = {
-			name: inputValue
-			// date: datestamp
+			name: inputValue,
+			date: `${displayDate}`
 		}
-		console.log("obj", filmObject);
 		dbRef.push(filmObject);
 
 		filmInput.value = "";
+	}
+});
+
+// delete film item when button is clicked
+filmItemContainer.addEventListener("click", (e) => {
+	if ((e.target.tagName === "I") || (e.target.tagName === "BUTTON")) {
+		let targetKey = e.target.closest(".filmItem").getAttribute("id");
+		db.ref(`${targetKey}`).remove();
 	}
 });
